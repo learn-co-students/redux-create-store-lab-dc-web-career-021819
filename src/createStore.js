6338a5197cb87b58a3b459d66e594521d6b8ce85
Mickey <micksheridan.24@gmail.com>
2019-05-09 16:46:02 -0400

    submit

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,23 @@
+import candyReducer from "./reducers/candyReducer";
+import countReducer from "./reducers/countReducer";
+
 export default function createStore(reducer) {
   // add your code here
+  let state;
+  const getState = function() {
+    return state;
+  };
+  const dispatch = function(action) {
+    state = reducer(state, action);
+    render();
+  };
+  dispatch("INIT");
+  return { getState, dispatch };
 }
 
 function render() {
-  const container = document.getElementById('container');
+  const container = document.getElementById("container");
 }
+
+// createStore(candyReducer);
+// createStore(countReducer);
